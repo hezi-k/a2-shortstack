e704d8f5e7c7de2b36a7d7bc628d22275fd88d1a
Glitch (a2-hezi-k) <none>
2020-09-17 02:36:54 +0000

    🚈🏯 Updated with Glitch

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-Assignment 2 - Short Stack: Basic Two-tier Web Application using HTML/CSS/JS and Node.js  
-===
+# Assignment 2 - Short Stack: Basic Two-tier Web Application using HTML/CSS/JS and Node.js
+
 ---
+
 ## WPI Football
+
 WPI Football seeks to address the need of organising pick up football in WPI. Currently the system is set up on messenger. Someone will send a message asking to play and people respond accordingly. However the system can be confusing and convoluted. I would really like to develop this website further. As a starter, I wanted to create a system where other players can check eachother out and see simple things like their name and their stats. I wanted to make this on a separate page however I could not figure out how to do this with the sever. You can see the commented out code in the respective files. I would like to make this more robust and let people add more stats or even profile pictures. For now a user is able to put in their username, password and some basic stats and this is displayed in a table below. However, I think this has a lot of potential.
 
 One other future I could not figure out was how to delete an entry, with a button. However, I think I need to spend more time on this.
 
 ## Technical Achievements
+
 - **Tech Achievement 1**: This single-page app utilizes a combination of forms and tables so that a user can submit data, and the app will display both the data and the result of the derived field. The form includes a username, password, nickname, preferred foot, and position.
 
-
 ### Design/Evaluation Achievements
+
 - **Student Interview 1**:
 
-Student Name: Lindberg Simpson
-Problems:
-Surprising Comments:
+Student Name: Lindberg Simpson  
+Problems:  
+Surprising Comments:  
 Changes to be made based on feedback:
 
 - **Student Interview 2**:
 
-Student Name: Akul Argawal
-Problems:
-Surprising Comments:
-Changes to be made based on feedback:
+Student Name: Akul Argawal  
+Problems:  
+Surprising Comments:  
+Changes to be made based on feedback:  
--- server.improved.js
+++ server.improved.js
@@ -1,13 +1,20 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
-  { id: 0, username: "Hezi", Password: "hello", Foot: "left", Position: "def", dateJoined: "2020-9-21" }
+  {
+    id: 0,
+    username: "Hezi",
+    Password: "hello",
+    Foot: "left",
+    Position: "def",
+    dateJoined: "2020-9-21"
+  }
 ];
 
 let playerPage = null;
@@ -15,70 +22,61 @@ fs.readFile("public/players.html", function(err, content) {
   playerPage = content;
 });
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )
-  }else if( request.method === 'POST' ){
-    handlePost( request, response )
+const server = http.createServer(function(request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 )
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/players") {
+    sendFile(response, "public/players.html");
+  } else if (request.url === "/game") {
+    sendFile(response, "public/game.html");
+  } else if (request.url === "/style.css") {
+    sendFile(response, "public/css/style.css");
+  } else {
+    sendFile(response, filename);
   }
-  else if( request.url === "/players") {
-    sendFile( response, "public/players.html")
-  }
-  else if( request.url === "/game") {
-    sendFile( response, "public/game.html");
-  }
-  else if(request.url === "/style.css") {
-    sendFile( response, "public/css/style.css");
-  }
-  else{
-    sendFile( response, filename )
-  }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function(request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data
-  })
+  request.on("data", function(data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("end", function() {
+    console.log(JSON.parse(dataString));
 
     // ... do something with the data here!!!
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end();
+  });
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename )
+const sendFile = function(response, filename) {
+  const type = mime.getType(filename);
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile(filename, function(err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
